663a0559725c30628d4eecbc779cd72ac55db086
Eman-Bman <ethanbergman0@gmail.com>
2025-01-29 16:44:23 -0500

    Create Bot-Test.js

---
+++ Bot-Test.js
@@ -0,0 +1,9 @@
+this.options = {
+  // see documentation for options reference
+  root_mode: "survival",
+  map_size: 30
+};
+
+this.tick = function(game) {
+  // do mod stuff here ; see documentation
+}
