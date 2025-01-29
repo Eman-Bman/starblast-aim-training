a39b58ac798b03c1a60de69febaba2ef901752d8
Eman-Bman <ethanbergman0@gmail.com>
2025-01-29 17:00:35 -0500

    Update Bot-Test.js

--- Bot-Test.js
+++ Bot-Test.js
@@ -182,309 +182,309 @@ const VOCABULARY = [
     {text: "",   icon: "\u{1D7CE}", key: "0"}
 ];
 //Button data
-var Regen_button = {
-  id: "Regen",
-  position: [68, 1, 3.5, 5.5],
-  clickable: true,
-  shortcut: "U",
-  visible: true,
-  components: [
-    { type:"box",position:[0,0,100,100],fill:"#456",stroke:"#CDE",width:2},
-    { type: "text",position:[10,35,80,30],value:"New Alien",color:"#CDE"},
-    { type: "text",position:[20,70,60,20],value:"[U]",color:"#CDE"}
-    ]
-};
+// var Regen_button = {
+//   id: "Regen",
+//   position: [68, 1, 3.5, 5.5],
+//   clickable: true,
+//   shortcut: "U",
+//   visible: true,
+//   components: [
+//     { type:"box",position:[0,0,100,100],fill:"#456",stroke:"#CDE",width:2},
+//     { type: "text",position:[10,35,80,30],value:"New Alien",color:"#CDE"},
+//     { type: "text",position:[20,70,60,20],value:"[U]",color:"#CDE"}
+//     ]
+// };
 
-var A_Speed_button = {
-  id: "A_Speed",
-  position: [72, 1, 3.5, 5.5],
-  clickable: true,
-  shortcut: "T",
-  visible: true,
-  components: [
-    { type:"box",position:[0,0,100,100],fill:"#456",stroke:"#CDE",width:2},
-    { type: "text",position:[10,35,80,30],value:"Change Ship",color:"#CDE"},
-    { type: "text",position:[20,70,60,20],value:"[T]",color:"#CDE"}
-    ]
-};
+// var A_Speed_button = {
+//   id: "A_Speed",
+//   position: [72, 1, 3.5, 5.5],
+//   clickable: true,
+//   shortcut: "T",
+//   visible: true,
+//   components: [
+//     { type:"box",position:[0,0,100,100],fill:"#456",stroke:"#CDE",width:2},
+//     { type: "text",position:[10,35,80,30],value:"Change Ship",color:"#CDE"},
+//     { type: "text",position:[20,70,60,20],value:"[T]",color:"#CDE"}
+//     ]
+// };
 
-var Return_button = {
-  id: "Return",
-  position: [76, 1, 3.5, 5.5],
-  clickable: true,
-  shortcut: "W",
-  visible: true,
-  components: [
-    { type:"box",position:[0,0,100,100],fill:"#456",stroke:"#CDE",width:2},
-    { type: "text",position:[10,35,80,30],value:"Return",color:"#CDE"},
-    { type: "text",position:[20,70,60,20],value:"[W]",color:"#CDE"}
-    ]
-};
+// var Return_button = {
+//   id: "Return",
+//   position: [76, 1, 3.5, 5.5],
+//   clickable: true,
+//   shortcut: "W",
+//   visible: true,
+//   components: [
+//     { type:"box",position:[0,0,100,100],fill:"#456",stroke:"#CDE",width:2},
+//     { type: "text",position:[10,35,80,30],value:"Return",color:"#CDE"},
+//     { type: "text",position:[20,70,60,20],value:"[W]",color:"#CDE"}
+//     ]
+// };
 
-var KillA_button = {
-  id: "KillA",
-  position: [64, 1, 3.5, 5.5],
-  clickable: true,
-  shortcut: "V",
-  visible: true,
-  components: [
-    { type:"box",position:[0,0,100,100],fill:"#456",stroke:"#CDE",width:2},
-    { type: "text",position:[10,35,80,30],value:"Kill Aliens",color:"#CDE"},
-    { type: "text",position:[20,70,60,20],value:"[V]",color:"#CDE"}
-    ]
-};
+// var KillA_button = {
+//   id: "KillA",
+//   position: [64, 1, 3.5, 5.5],
+//   clickable: true,
+//   shortcut: "V",
+//   visible: true,
+//   components: [
+//     { type:"box",position:[0,0,100,100],fill:"#456",stroke:"#CDE",width:2},
+//     { type: "text",position:[10,35,80,30],value:"Kill Aliens",color:"#CDE"},
+//     { type: "text",position:[20,70,60,20],value:"[V]",color:"#CDE"}
+//     ]
+// };
 
-var spectate_button = {
-  id: "spectate",
-  position: [60, 1, 3.5, 5.5],
-  clickable: true,
-  shortcut: "",
-  visible: true,
-  components: [
-    { type:"box",position:[0,0,100,100],fill:"#456",stroke:"#CDE",width:2},
-    { type: "text",position:[10,35,80,30],value:"Spectate",color:"#CDE"},
-    { type: "text",position:[20,70,60,20],value:"[(click)]",color:"#CDE"}
-    ]
-};
-//button functions
-var Regen = function(ship) {
-  if ((((ship.x-250)*(ship.x-250))+((ship.y-250)*(ship.y-250)))<=10000 && ship.type!=607)
-  {
-  game.addAlien({x:250,y:250,code:19,level:0,points:0,crystal_drop:720});
-  }
-} ;
+// var spectate_button = {
+//   id: "spectate",
+//   position: [60, 1, 3.5, 5.5],
+//   clickable: true,
+//   shortcut: "",
+//   visible: true,
+//   components: [
+//     { type:"box",position:[0,0,100,100],fill:"#456",stroke:"#CDE",width:2},
+//     { type: "text",position:[10,35,80,30],value:"Spectate",color:"#CDE"},
+//     { type: "text",position:[20,70,60,20],value:"[(click)]",color:"#CDE"}
+//     ]
+// };
+// //button functions
+// var Regen = function(ship) {
+//   if ((((ship.x-250)*(ship.x-250))+((ship.y-250)*(ship.y-250)))<=10000 && ship.type!=607)
+//   {
+//   game.addAlien({x:250,y:250,code:19,level:0,points:0,crystal_drop:720});
+//   }
+// } ;
 
-var KillA = function(ship) {
-  if ((((ship.x-250)*(ship.x-250))+((ship.y-250)*(ship.y-250)))<=10000 && ship.type!=607)
-  {
-    for (var k=game.aliens.length-1;k>-1;k--)
-    {
-    // game.aliens[k].set({crystal_drop:0});
-    game.aliens[k].set({kill:true});
-    }
-  }
-} ;
+// var KillA = function(ship) {
+//   if ((((ship.x-250)*(ship.x-250))+((ship.y-250)*(ship.y-250)))<=10000 && ship.type!=607)
+//   {
+//     for (var k=game.aliens.length-1;k>-1;k--)
+//     {
+//     // game.aliens[k].set({crystal_drop:0});
+//     game.aliens[k].set({kill:true});
+//     }
+//   }
+// } ;
 
-var A_Speed = function(ship) {
-  if (((ship.x*ship.x)+(ship.y*ship.y))<=8100 && ship.type!=607)
-  {
-    ship.set({type: (ship.type - 600) % 3 + 601});
-    ship.set({stats:88888888, crystals:719, shield: 1000});
-  }
-};
+// var A_Speed = function(ship) {
+//   if (((ship.x*ship.x)+(ship.y*ship.y))<=8100 && ship.type!=607)
+//   {
+//     ship.set({type: (ship.type - 600) % 3 + 601});
+//     ship.set({stats:88888888, crystals:719, shield: 1000});
+//   }
+// };
 
-var Return = function(ship) {
-  if (ship.type>=604 && ship.type!=607)
-  {
-    ship.set({type:(ship.type-3)});
-  }
-  //ship.set({type:605});
-  ship.set({x:0,y:0,vx:0,vy:0,shield:3000,stats:88888888,crystals:719});
-  shipCount();
-  COD=game.step+30;
-};
+// var Return = function(ship) {
+//   if (ship.type>=604 && ship.type!=607)
+//   {
+//     ship.set({type:(ship.type-3)});
+//   }
+//   //ship.set({type:605});
+//   ship.set({x:0,y:0,vx:0,vy:0,shield:3000,stats:88888888,crystals:719});
+//   shipCount();
+//   COD=game.step+30;
+// };
 
-var Spectate = function(ship) {
-  if (ship.type==607)
-  {
-    ship.set({x:0,y:0,type:601,vx:0,vy:0,shield:300,stats:88888888,crystals:719,collider:true});
-  }
-  else
-  {
-    if ((((ship.x)*(ship.x))+((ship.y)*(ship.y)))<=8100)
-    {
-    ship.set({type:607,shield:69,stats:88888888,crystals:0,collider:false});
-    }
-  }
-};
-//custom functions
+// var Spectate = function(ship) {
+//   if (ship.type==607)
+//   {
+//     ship.set({x:0,y:0,type:601,vx:0,vy:0,shield:300,stats:88888888,crystals:719,collider:true});
+//   }
+//   else
+//   {
+//     if ((((ship.x)*(ship.x))+((ship.y)*(ship.y)))<=8100)
+//     {
+//     ship.set({type:607,shield:69,stats:88888888,crystals:0,collider:false});
+//     }
+//   }
+// };
+// //custom functions
 
-var sendUI = function(ship, UI) {
-  if (ship !== null && typeof ship.setUIComponent == "function") {
-    if (UI.visible || UI.visible === null) ship.setUIComponent(UI);
-    else ship.setUIComponent({id: UI.id, position: [0,0,0,0], visible: false});
-  }
-};
+// var sendUI = function(ship, UI) {
+//   if (ship !== null && typeof ship.setUIComponent == "function") {
+//     if (UI.visible || UI.visible === null) ship.setUIComponent(UI);
+//     else ship.setUIComponent({id: UI.id, position: [0,0,0,0], visible: false});
+//   }
+// };
 
-var shipCount = function() {
-  area1=0;
-  a1ships=[];
-  area2=0;
-  area3=0;
-  area4=0;
-  for (var i=0;i<game.ships.length;i++)
-  {
-    if (game.ships[i].type!=607)
-    {
-      if (((((game.ships[i].x-250)*(game.ships[i].x-250))+((game.ships[i].y+250)*(game.ships[i].y+250))))<=10000)
-      {
-        area4=area4+1;
-      }
-      if (((((game.ships[i].x+250)*(game.ships[i].x+250))+((game.ships[i].y+250)*(game.ships[i].y+250))))<=10000)
-      {
-        area3=area3+1;
-      }
-      if (((((game.ships[i].x-250)*(game.ships[i].x-250))+((game.ships[i].y-250)*(game.ships[i].y-250))))<=10000)
-      {
-        area2=area2+1;
-      }
-      if (((((game.ships[i].x+250)*(game.ships[i].x+250))+((game.ships[i].y-250)*(game.ships[i].y-250))))<=14400)
-      {
-        area1=area1+1;
-        a1ships.push(i);
-      }
-    }
-  }
-  if (area4===0)
-  {
-    a4ship=[];
-  }
-  if (area3===0)
-  {
-    a3ship=[];
-  }
-};
+// var shipCount = function() {
+//   area1=0;
+//   a1ships=[];
+//   area2=0;
+//   area3=0;
+//   area4=0;
+//   for (var i=0;i<game.ships.length;i++)
+//   {
+//     if (game.ships[i].type!=607)
+//     {
+//       if (((((game.ships[i].x-250)*(game.ships[i].x-250))+((game.ships[i].y+250)*(game.ships[i].y+250))))<=10000)
+//       {
+//         area4=area4+1;
+//       }
+//       if (((((game.ships[i].x+250)*(game.ships[i].x+250))+((game.ships[i].y+250)*(game.ships[i].y+250))))<=10000)
+//       {
+//         area3=area3+1;
+//       }
+//       if (((((game.ships[i].x-250)*(game.ships[i].x-250))+((game.ships[i].y-250)*(game.ships[i].y-250))))<=10000)
+//       {
+//         area2=area2+1;
+//       }
+//       if (((((game.ships[i].x+250)*(game.ships[i].x+250))+((game.ships[i].y-250)*(game.ships[i].y-250))))<=14400)
+//       {
+//         area1=area1+1;
+//         a1ships.push(i);
+//       }
+//     }
+//   }
+//   if (area4===0)
+//   {
+//     a4ship=[];
+//   }
+//   if (area3===0)
+//   {
+//     a3ship=[];
+//   }
+// };
 
-var remAst4 = function()
-{
-  for (var l=game.asteroids.length-1;l>-1;l--)
-  {
-    echo(l);
-    if (((((game.asteroids[l].x-250)*(game.asteroids[l].x-250))+((game.asteroids[l].y+250)*(game.asteroids[l].y+250))))<=10000)
-    {
-      game.asteroids[l].set({kill:true});
-    }
-  }
-};
+// var remAst4 = function()
+// {
+//   for (var l=game.asteroids.length-1;l>-1;l--)
+//   {
+//     echo(l);
+//     if (((((game.asteroids[l].x-250)*(game.asteroids[l].x-250))+((game.asteroids[l].y+250)*(game.asteroids[l].y+250))))<=10000)
+//     {
+//       game.asteroids[l].set({kill:true});
+//     }
+//   }
+// };
 
-var remAst3 = function()
-{
-  for (var l=game.asteroids.length-1;l>-1;l--)
-  {
-    echo(l);
-    if (((((game.asteroids[l].x+250)*(game.asteroids[l].x+250))+((game.asteroids[l].y+250)*(game.asteroids[l].y+250))))<=10000)
-    {
-      game.asteroids[l].set({kill:true});
-    }
-  }
-};
+// var remAst3 = function()
+// {
+//   for (var l=game.asteroids.length-1;l>-1;l--)
+//   {
+//     echo(l);
+//     if (((((game.asteroids[l].x+250)*(game.asteroids[l].x+250))+((game.asteroids[l].y+250)*(game.asteroids[l].y+250))))<=10000)
+//     {
+//       game.asteroids[l].set({kill:true});
+//     }
+//   }
+// };
 
-var newCircle = function()
-{
-  echo("newCircle")
-  Next4=game.step+Math.floor(1000000/(game.step-startTime4+3333));
-  for (var l=0;l<a4ship.length;l++) {
-    angle=360*Math.random();
-    let vx=0.05*(game.ships[a4ship[l]].x-(85*Math.cos(angle)+245));
-    let vy=0.05*(game.ships[a4ship[l]].y-(85*Math.sin(angle)-245));
-    let vt=Math.sqrt((vx*vx)+(vy*vy));
-    let vg=1.5-(10000/((game.step-startTime4)+9000));
-    let size=((game.step-startTime4)/720)+10;
-    vx=vx/(vt/vg);
-    vy=vy/(vt/vg);
-    game.addAsteroid({x:(85*Math.cos(angle)+245),y:(85*Math.sin(angle)-245),size:size,vx:(vx),vy:(vy)});
-    game.ships[a4ship[l]].set({score:game.ships[a4ship[l]].score+(Math.floor(1000000/(game.step-startTime4+3333)))});
-  }
-};
+// var newCircle = function()
+// {
+//   echo("newCircle")
+//   Next4=game.step+Math.floor(1000000/(game.step-startTime4+3333));
+//   for (var l=0;l<a4ship.length;l++) {
+//     angle=360*Math.random();
+//     let vx=0.05*(game.ships[a4ship[l]].x-(85*Math.cos(angle)+245));
+//     let vy=0.05*(game.ships[a4ship[l]].y-(85*Math.sin(angle)-245));
+//     let vt=Math.sqrt((vx*vx)+(vy*vy));
+//     let vg=1.5-(10000/((game.step-startTime4)+9000));
+//     let size=((game.step-startTime4)/720)+10;
+//     vx=vx/(vt/vg);
+//     vy=vy/(vt/vg);
+//     game.addAsteroid({x:(85*Math.cos(angle)+245),y:(85*Math.sin(angle)-245),size:size,vx:(vx),vy:(vy)});
+//     game.ships[a4ship[l]].set({score:game.ships[a4ship[l]].score+(Math.floor(1000000/(game.step-startTime4+3333)))});
+//   }
+// };
 
-var customObj = function()
-{
-  game.removeObject()
+// var customObj = function()
+// {
+//   game.removeObject()
   
-  game.setObject({
-  id: "center",
-  type: center,
-  position: {x:0,y:0,z:0},
-  rotation: {x:0,y:0,z:0},
-  scale: {x:1,y:1,z:1}
-  }) ;
+//   game.setObject({
+//   id: "center",
+//   type: center,
+//   position: {x:0,y:0,z:0},
+//   rotation: {x:0,y:0,z:0},
+//   scale: {x:1,y:1,z:1}
+//   }) ;
   
-  game.setObject({
-  id: "startArea",
-  type: startArea,
-  position: {x:-160,y:-245,z:0},
-  rotation: {x:0,y:0,z:0},
-  scale: {x:1,y:1,z:1}
-  }) ;
+//   game.setObject({
+//   id: "startArea",
+//   type: startArea,
+//   position: {x:-160,y:-245,z:0},
+//   rotation: {x:0,y:0,z:0},
+//   scale: {x:1,y:1,z:1}
+//   }) ;
   
-  game.setObject({
-  id: "endArea",
-  type: endArea,
-  position: {x:-335,y:-245,z:0},
-  rotation: {x:0,y:0,z:0},
-  scale: {x:1,y:1,z:1}
-  }) ;
+//   game.setObject({
+//   id: "endArea",
+//   type: endArea,
+//   position: {x:-335,y:-245,z:0},
+//   rotation: {x:0,y:0,z:0},
+//   scale: {x:1,y:1,z:1}
+//   }) ;
   
-  game.setObject({
-  id: "desc4",
-  type: desc4,
-  position: {x:30,y:-45,z:-1},
-  rotation: {x:0,y:0,z:0},
-  scale: {x:0.2,y:0.2,z:0.01}
-  }) ;
+//   game.setObject({
+//   id: "desc4",
+//   type: desc4,
+//   position: {x:30,y:-45,z:-1},
+//   rotation: {x:0,y:0,z:0},
+//   scale: {x:0.2,y:0.2,z:0.01}
+//   }) ;
   
-  game.setObject({
-  id: "desc3",
-  type: desc3,
-  position: {x:-30,y:-45,z:-1},
-  rotation: {x:0,y:0,z:0},
-  scale: {x:0.2,y:0.2,z:0.01}
-  }) ;
+//   game.setObject({
+//   id: "desc3",
+//   type: desc3,
+//   position: {x:-30,y:-45,z:-1},
+//   rotation: {x:0,y:0,z:0},
+//   scale: {x:0.2,y:0.2,z:0.01}
+//   }) ;
   
-  if (area1>(a1cap-1))
-  {
-    game.setObject({
-    id: "blocked1",
-    type: blocked,
-    position: {x:-50,y:50,z:0},
-    rotation: {x:0,y:0,z:0},
-    scale: {x:1,y:1,z:1}
-    }) ;
-  }
+//   if (area1>(a1cap-1))
+//   {
+//     game.setObject({
+//     id: "blocked1",
+//     type: blocked,
+//     position: {x:-50,y:50,z:0},
+//     rotation: {x:0,y:0,z:0},
+//     scale: {x:1,y:1,z:1}
+//     }) ;
+//   }
   
-  if (area2>(a2cap-1))
-  {
-    game.setObject({
-    id: "blocked2",
-    type: blocked,
-    position: {x:50,y:50,z:0},
-    rotation: {x:0,y:0,z:0},
-    scale: {x:1,y:1,z:1}
-    }) ;
-  }
+//   if (area2>(a2cap-1))
+//   {
+//     game.setObject({
+//     id: "blocked2",
+//     type: blocked,
+//     position: {x:50,y:50,z:0},
+//     rotation: {x:0,y:0,z:0},
+//     scale: {x:1,y:1,z:1}
+//     }) ;
+//   }
   
-  if (area3>(a3cap-1))
-  {
-    game.setObject({
-    id: "blocked3",
-    type: blocked,
-    position: {x:-50,y:-50,z:0},
-    rotation: {x:0,y:0,z:0},
-    scale: {x:1,y:1,z:1}
-    }) ;
-  }
+//   if (area3>(a3cap-1))
+//   {
+//     game.setObject({
+//     id: "blocked3",
+//     type: blocked,
+//     position: {x:-50,y:-50,z:0},
+//     rotation: {x:0,y:0,z:0},
+//     scale: {x:1,y:1,z:1}
+//     }) ;
+//   }
 
-  if (area4>(a4cap-1))
-  {
-    game.setObject({
-    id: "blocked4",
-    type: blocked,
-    position: {x:50,y:-50,z:0},
-    rotation: {x:0,y:0,z:0},
-    scale: {x:1,y:1,z:1}
-    }) ;
-  }
-};
+//   if (area4>(a4cap-1))
+//   {
+//     game.setObject({
+//     id: "blocked4",
+//     type: blocked,
+//     position: {x:50,y:-50,z:0},
+//     rotation: {x:0,y:0,z:0},
+//     scale: {x:1,y:1,z:1}
+//     }) ;
+//   }
+// };
 
-var newLine = function()
-{
-  Next3=game.step+Math.floor(1000000/(game.step-startTime3+3333));
-  for (var l=0;l<a3ship.length;l++) {
-    let vg=1.5-(10000/((game.step-startTime3)+9000));
-    game.addAsteroid({x:-155,y:(20*Math.random()-255),size:15,vx:(-vg),vy:0});
-    game.ships[a3ship[l]].set({score:game.ships[a3ship[l]].score+(Math.floor(1000000/(game.step-startTime3+3333)))});
-  }
-}
+// var newLine = function()
+// {
+//   Next3=game.step+Math.floor(1000000/(game.step-startTime3+3333));
+//   for (var l=0;l<a3ship.length;l++) {
+//     let vg=1.5-(10000/((game.step-startTime3)+9000));
+//     game.addAsteroid({x:-155,y:(20*Math.random()-255),size:15,vx:(-vg),vy:0});
+//     game.ships[a3ship[l]].set({score:game.ships[a3ship[l]].score+(Math.floor(1000000/(game.step-startTime3+3333)))});
+//   }
+// }
 
 //objects
 // var redX = {
